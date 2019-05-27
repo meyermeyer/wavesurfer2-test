60234b8defa2f5394ab222b2683909e90620d013
Meyer <ehlmeyer@gmail.com>
2019-05-26 21:52:55 -0500

    hovering over region shows tag in console log

--- src/components/Waveform/Waveform.js
+++ src/components/Waveform/Waveform.js
@@ -10,7 +10,7 @@ import {connect} from 'react-redux'
 
 class Waveform extends React.Component {
     state = {
-        regionsList: []
+        regionsArray: []
     }
 
     // wavesurfer = WaveSurfer.create({
@@ -47,23 +47,39 @@ class Waveform extends React.Component {
             color: this.randomColor(.1)
         });
     }
+
+    handleHover = (region) => {
+        console.log('hovering over', region.data.regionTag);
+        
+    }
    
-    saveRegions = () => {
+    saveRegions = (region) => {
         // alert('you created a region');
         let regionTag = prompt("Tag")
         let regionNotes = prompt("Notes")
- 
+        console.log('region:', region);
+        //update 'region' created by clicking to include user's data
+        region.update({
+            data: {
+                regionTag,
+                regionNotes
+            }
+        })
+        console.log('updated region', region);
         console.log('prompt responses:', regionTag, regionNotes);
-        
-
+        console.log(this.wavesurfer.regions);
+       
+        //add regions.list objects to arrat
         let regionsArray = []
         for (let i in this.wavesurfer.regions.list){
             regionsArray.push(this.wavesurfer.regions.list[i])
         }
         console.log('in saveRegions', this.wavesurfer.regions.list);
+        console.log('regionsArray', regionsArray);
+        
         this.wavesurfer.regions.list && this.setState({
             ...this.state,
-            regionsList: regionsArray
+            regionsArray: regionsArray
         })
         this.props.dispatch({type:"SEND_REGIONS", payload: regionsArray, })
     }
@@ -94,6 +110,7 @@ class Waveform extends React.Component {
         this.wavesurfer.load(dogBarking);
         console.log(this.wavesurfer.regions);
         this.wavesurfer.on('region-update-end', this.saveRegions);
+        this.wavesurfer.on('region-mouseenter',this.handleHover)
     }
     componentWillUnmount() {
 
